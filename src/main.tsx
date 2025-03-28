0fdedca47ac27967d8d2b567f2b9bd90be8d2472
JOELATUHAURE <100364574+JOELATUHAURE@users.noreply.github.com>
2025-03-28 14:47:02 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
-import 'swiper/swiper-bundle.min.css';
+import 'swiper/css';  
+import 'swiper/css/navigation';  
+import 'swiper/css/pagination';  
+
 
 import './index.css';
 
