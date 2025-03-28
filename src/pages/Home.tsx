59a6d0d5995443bc7fe49bd2382843295aa1d7ce
JOELATUHAURE <100364574+JOELATUHAURE@users.noreply.github.com>
2025-03-28 14:49:46 +0300

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,10 @@ import { motion } from 'framer-motion';
 import { Link } from 'react-router-dom';
 import { ArrowRight, Star, Users, Calendar, Award } from 'lucide-react';
 import { Swiper, SwiperSlide } from 'swiper/react'; // Import Swiper
-import 'swiper/swiper-bundle.min.css'; // Import Swiper styles
+import 'swiper/css';  
+import 'swiper/css/navigation';  
+import 'swiper/css/pagination';  
+
 
 const Home = () => {
   return (
