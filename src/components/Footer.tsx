659c03ab6186207fa12fda5f5bf049b48eb5e933
JOELATUHAURE <100364574+JOELATUHAURE@users.noreply.github.com>
2025-03-29 20:12:38 +0300

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Phone, Mail, MapPin, Facebook, Instagram, Youtube } from 'lucide-react';
-import { FaTiktok } from "@fortawesome/free-brands-svg-icons";
+import { FaTiktok } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 
