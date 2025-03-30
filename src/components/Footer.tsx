607d656e9a434bb7faecbaff0eb2c886f32f1fe7
JOELATUHAURE <100364574+JOELATUHAURE@users.noreply.github.com>
2025-03-31 00:28:25 +0300

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -89,7 +89,7 @@ const Footer = () => {
 
         <div className="border-t border-gray-800 mt-12 pt-8">
           <p className="text-center text-gray-400">
-            © {new Date().getFullYear()} Tinah Events. All rights reserved.
+            © {new Date().getFullYear()} Tinah Decor Events Ug. All rights reserved.
           </p>
         </div>
       </div>
