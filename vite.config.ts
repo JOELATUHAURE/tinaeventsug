e00c7bbd269d1598fa0ad926e95852a7cf900798
JOELATUHAURE <100364574+JOELATUHAURE@users.noreply.github.com>
2025-04-02 00:58:18 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/',  // This is crucial for handling client-side routing
   plugins: [react()],
   resolve: {
     alias: {
