f15e5f2e03d8c3d8c7cbf86365b2edc3add0d331
JOELATUHAURE <100364574+JOELATUHAURE@users.noreply.github.com>
2025-04-01 23:53:41 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'], // Files to be scanned for classes
   theme: {
     extend: {
       colors: {
